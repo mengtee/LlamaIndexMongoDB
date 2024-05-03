6654220598b7ba10f6c725585d74b851a8584b71
xoxo2002 <63912996+xoxo2002@users.noreply.github.com>
2024-05-03 17:01:26 +0800

    Added sumarry section and updated podcast list

--- frontend/app/components/chat-section.tsx
+++ frontend/app/components/chat-section.tsx
@@ -47,11 +47,11 @@ export default function ChatandPodcastSection() {
       <div>
         <h4 className="text-3xl font-thin mb-4">Catch up with last week's episode.</h4>
         <div className=" h-[70vh] flex-1 overflow-scroll ">
-          <iframe className="rounded-md scale-90" src="https://omny.fm/shows/market-watch/us-corporate-results-driving-market-sentiment/embed?style=Cover" width="100%" height="160px" allow="autoplay; clipboard-write" frameborder="0" title="US Corporate Results Driving Market Sentiment"></iframe>
-          <iframe className="rounded-md scale-90" src="https://omny.fm/shows/market-watch/the-fed-still-cutting-rates/embed?style=Cover" width="100%" height="160px"  allow="autoplay; clipboard-write" frameborder="0" title="The Fed Still Cutting Rates"></iframe>
-          <iframe className="rounded-md scale-90" src="https://omny.fm/shows/market-watch/china-evs-ready-for-global-domination/embed?style=Cover" width="100%" height="160px" allow="autoplay; clipboard-write" frameborder="0" title="China EVs Ready For Global Domination?"></iframe>
-          <iframe className="rounded-md scale-90" src="https://omny.fm/shows/market-watch/us-earnings-season-matters/embed?style=Cover" width="100%" height="160px" allow="autoplay; clipboard-write" frameborder="0" title="US Earnings Season Matters"></iframe>
-          <iframe className="rounded-md scale-90" src="https://omny.fm/shows/market-watch/indonesia-still-the-land-of-opportunity/embed?style=Cover" width="100%" height="160px" allow="autoplay; clipboard-write" frameborder="0" title="Indonesia: Still the Land of Opportunity "></iframe>
+          <iframe className="rounded-md scale-90" src="https://omny.fm/shows/market-watch/play-the-waiting-game-with-markets/embed?in_playlist=podcast&style=Cover" width="100%" height="180" allow="autoplay; clipboard-write" frameborder="0" title="Play The Waiting Game With Markets "></iframe>
+          <iframe className="rounded-md scale-90" src="https://omny.fm/shows/market-watch/us-earnings-season-matters/embed?in_playlist=podcast&style=Cover" width="100%" height="180" allow="autoplay; clipboard-write" frameborder="0" title="US Earnings Season Matters"></iframe>
+          <iframe className="rounded-md scale-90" src="https://omny.fm/shows/market-watch/the-fed-still-cutting-rates/embed?in_playlist=podcast&style=Cover" width="100%" height="180" allow="autoplay; clipboard-write" frameborder="0" title="The Fed Still Cutting Rates"></iframe>
+          <iframe className="rounded-md scale-90" src="https://omny.fm/shows/market-watch/us-corporate-results-driving-market-sentiment/embed?in_playlist=podcast&style=Cover" width="100%" height="180" allow="autoplay; clipboard-write" frameborder="0" title="US Corporate Results Driving Market Sentiment"></iframe>
+          <iframe className="rounded-md scale-90" src="https://omny.fm/shows/market-watch/the-magnificent-7-are-now-the-fantastic-four/embed?in_playlist=podcast&style=Cover" width="100%" height="180" allow="autoplay; clipboard-write" frameborder="0" title="The Magnificent 7 Are Now The Fantastic Four "></iframe>
         </div>
       </div>
       
